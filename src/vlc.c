018cddcda4c74972b6894de0eede09e7d9f9c584
Charlie <guoce.ecnu@gmail.com>
2017-12-07 11:39:52 -0600

    vlc.c interface changes. Added more comment.

--- src/vlc.c
+++ src/vlc.c
@@ -13,7 +13,7 @@
 
 static void diep(char *s) {perror(s); exit(1);}
 
-static void _getCmdStr(char *cmdStr, vlcCmd_t *op);
+static void _getCmdStr(char *cmdStr, vlcOpCode_t op, uint32_t para);
 
 socket_t vlcConnect(char *path) {
   socket_t s;
@@ -36,23 +36,74 @@ socket_t vlcConnect(char *path) {
   return s;
 }
 
-void vlcSendCmd(socket_t s, vlcCmd_t *cmd) {
+void vlcSetStatus(vlcInterface_t *sock, vlcOpCode_t op, uint32_t para) {
   char cmdStr[VLC_MAX_CMD_LEN];
-  _getCmdStr(cmdStr, cmd);
+  _getCmdStr(cmdStr, op, para);
   printf("Sending: %s\n", cmdStr);
-  if (send(s, cmdStr, strlen(cmdStr), 0) == -1) 
+  if (send(sock->s, cmdStr, strlen(cmdStr), 0) == -1) 
     diep("vlc send()");
 }
 
-static void _getCmdStr(char *cmdStr, vlcCmd_t *cmd) {
+static void _getCmdStr(char *cmdStr, vlcOpCode_t op, uint32_t para) {
   memset(cmdStr, 0, VLC_MAX_CMD_LEN);
-  switch(cmd->op) {
+  switch(op) {
     case OP_PAUSE: 
       sprintf(cmdStr, "pause"); break;
     case OP_PLAY:
       sprintf(cmdStr, "pause"); break;
     case OP_SEEK:
-      sprintf(cmdStr, "seek %d", cmd->para);
+      sprintf(cmdStr, "seek %d", para);
       break;
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/// Get lenght of the playing video in second.
+////////////////////////////////////////////////////////////////////////////////
+uint32_t vlcGetLength(vlcInterface_t *sock) {
+  //TODO send "get_length" command to VLC and read back the lenght
+  return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Get time elapsed since the begining of the video in second.
+////////////////////////////////////////////////////////////////////////////////
+uint32_t vlcGetTime(vlcInterface_t *sock) {
+  //TODO send "get_time" command to VLC and read back current time
+  return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Get current playback status, including whether it's playing or paused 
+///   and time elapsed.
+////////////////////////////////////////////////////////////////////////////////
+void vlcGetStatus(vlcInterface_t *sock, vlcStatus_t *stat) {
+  //TODO send "status" to VLC to get current playing status.
+  //  This can be a little tricky since it seems the return of this command is
+  //  not consistent. For example, if "status" is the very first command ever
+  //  get sent to a brand-newly opened VLC, it doesn't return current playback
+  //  status. Therefore we may need to consider send some dummy command to VLC
+  //  as a part of VLC RC sock connecting process. 
+  
+  
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Get whether a video is opened in VLC. 
+////////////////////////////////////////////////////////////////////////////////
+bool_t vlcGetIsOpened(vlcInterface_t *sock) {
+  //TODO
+  // This fucntion gets whether a file is opened in VLC,
+  // which mathches the ruturn of "is_playing".
+  // CAUTION: Return true as long as a file is opened in VLC, even it's paused.
+  return FALSE;
+}
+
+void vlcSetStatus(vlcStatus_t *stat){
+  //TODO
+  // The program shouldn't make any assumption of the current status of VLC
+  // It's important to check current VLC status before set the status since command "pause"
+  // toggles play and pause.
+}
+
--- src/vlc.h
+++ src/vlc.h
@@ -3,6 +3,14 @@
 #ifndef VLC_H
 #define VLC_H
 
+#include "common.h"
+#include <poll.h>
+
+typedef struct {
+  socket_t s;
+  struct pollfd pfds[1];
+}vlcInterface_t;
+
 socket_t vlcConnect(char *path);
 void vlcSendCmd(socket_t s, vlcCmd_t *cmd);
 
