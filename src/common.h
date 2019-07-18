ab4588bf1089a11dbd3ec753e6e1ea8713c68d20
Ce Guo <ceguo@wirelessprv-10-193-191-15.near.illinois.edu>
2019-07-18 18:54:25 -0500

    Added debugging code Added a new message analyze function for status poll, which returns valid status only when the message is generated by vlc proactively (which means there was a user input through the gui). That way we can ignore the reponses spitted out by vlc after it gets a command from out program.

--- src/common.h
+++ src/common.h
@@ -19,5 +19,11 @@ typedef int socket_t;
 #define     TRUE    1
 #define     FALSE   0
 
+#define     DEBUG_MODE  TRUE
+#define     DEBUG_SINK  stdout
+#define     debugf(fmt, ...)        {if(DEBUG_MODE) fprintf(DEBUG_SINK, (fmt), __VA_ARGS__);}
+#define     debug_str(str)          debugf("%s\n", (str))
+#define     debug_ent_func(func)    debugf("%s entry\n", (func))  
+#define     debug_exi_func(func)    debugf("%s exit\n", (func))
 
 #endif // COMMON_H
